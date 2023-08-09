83f15f8d3cb3e1b2a666d95f41188ed931350e13
mcfrojd <mcfrojd@gmail.com>
2023-08-09 14:56:55 +0200

    Updated uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SERVICE_NAME=$(basename $SCRIPT_DIR)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
+DAEMON_NAME=${SCRIPT_DIR##*/}
 
-rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-shelly-em-smartmeter')
+rm /service/$DAEMON_NAME
+kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
+kill $(pgrep -f 'supervise $DAEMON_NAME')
 chmod a-x $SCRIPT_DIR/service/run
-./restart.sh
+$SCRIPT_DIR/restart.sh
