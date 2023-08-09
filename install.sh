37f8d96542903fe5882f757d9ef891eb919ba84b
mcfrojd <mcfrojd@gmail.com>
2023-08-09 15:00:40 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SERVICE_NAME=$(basename $SCRIPT_DIR)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  )
+DAEMON_NAME=${SCRIPT_DIR##*/}
 
 # set permissions for script files
 chmod a+x $SCRIPT_DIR/restart.sh
@@ -13,7 +13,7 @@ chmod a+x $SCRIPT_DIR/service/run
 chmod 755 $SCRIPT_DIR/service/run
 
 # create sym-link to run script in deamon
-ln -s $SCRIPT_DIR/service /service/$SERVICE_NAME
+ln -s $SCRIPT_DIR/service /service/$DAEMON_NAME
 
 # add install-script to rc.local to be ready for firmware update
 filename=/data/rc.local
